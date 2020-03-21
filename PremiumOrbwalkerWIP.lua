04edf2fc536d24f9df10174fb6faeeb57d75c1a8
Ark223 <Ark223Neww@gmail.com>
2020-03-21 15:23:20 +0100

    Orbwalker

--- PremiumOrbwalkerWIP.lua
+++ PremiumOrbwalkerWIP.lua
@@ -364,7 +364,7 @@ function PremiumOrb:GetAutoAttackRange(unit)
 	local range = myHero.range + (myHero.boundingRadius or 65)
 	if unit and self:IsValid(unit) then
 		if myHero.charName == "Aphelios" and self:HasBuff(unit,
-			{"aphelioscalibrumbonusrangedebuff"}) > 0 then return MathHuge
+			{"aphelioscalibrumbonusrangedebuff"}) > 0 then return 1800
 		elseif myHero.charName == "Caitlyn" and
 			self:HasBuff(myHero, {"caitlynyordletrapinternal"}) > 0 then
 				range = range + 650
